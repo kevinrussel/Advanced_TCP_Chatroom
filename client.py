ff12c060f71e895fbf13525e700691fab2a6343e
kevin russel <kzone159@gmail.com>
2026-01-16 07:43:23 -0600

    adding old tcp cold to build off of

--- client.py
+++ client.py
@@ -1,22 +1,43 @@
 import threading
 import socket
 import threading
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1',8080))
+
+def write_message():
+    while True:            
+        message = input()
+        message = message.encode('utf-8')
+        client.sendall(message)
 
 
 
+# We are going to be making a client right now for our server
+def recieve_message():
+    
+    while True:
+        message = client.recv(4096) 
+        message = message.decode('utf-8')
+        print(f"{message}")
+        if message == "Goodbye from server":
+            break
+        elif len(message) == 0:
+            print("DISCONNECTED FROM SERVER")
+            break
+    return
+    
+
+def create_connection():
+    message = threading.Thread(target=write_message, args=(), daemon=True)
+    message.start()
+    recieve = threading.Thread(target=recieve_message, args=())
+    recieve.start()
+    recieve.join()
+    return
+    
 
 
 
-
-
-
-
-def main():
-    print("hello world")
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('127.0.0.1',8080))
-
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    create_connection()
+    
--- server.py
+++ server.py
@@ -1,9 +1,97 @@
-import threading
+
+
 import socket
+import threading
+
+
+
+NICKNAME = []
+CONNECTION = []
+
+def broad_cast_message (client_socket, message):
+    client_socket.sendall(message.encode('utf-8'))
+    return
 
 
 
 
+def handle_method(client_socket, message):
+    broad_cast_message(client_socket, message)
+    while True:
+        try:
+            message = client_socket.recv(4096)
+            if message == b'':
+                raise OSError
+            message = message.decode("utf-8")
+            
+            name = ''
+            for index,client in enumerate(CONNECTION):
+                if (client == client_socket):
+                    name = NICKNAME[index]
+            if message == "exit":
+                print(f"{name} is leaving from server")
+                for index, client in enumerate(CONNECTION):
+                    if(client != client_socket):
+                        broad_cast_message(client,f"{name} has disconnected!")
+                    else:
+                        broad_cast_message(client, "Goodbye from server")
+                CONNECTION.remove(client_socket)
+                NICKNAME.remove(name)
+                client_socket.close()
+                break
+            else:
+                message = f"{name}: "+ message
+                for index, client in enumerate(CONNECTION):
+                    if( client!= client_socket ):
+                        broad_cast_message(client,message)
+        except OSError:
+            name = ''
+            for index, client in enumerate(CONNECTION):
+                if client == client_socket:
+                    name = NICKNAME[index]
+            CONNECTION.remove(client_socket)
+            NICKNAME.remove(name)
+            client_socket.close()
+            for index, client in enumerate(CONNECTION):
+                broad_cast_message(client,f"{name} has lost connection with server")
+            
+
+# TODO: Handle in a try catch maybe?
+def new_connection_message(client_socket, client_name):
+
+    for index, connections in enumerate(CONNECTION):
+        if connections != client_socket:
+            message = f"{client_name} has joined the chat!"
+            broad_cast_message(connections,message)
+
+
+## TODO: Handle in a try catch.
+def handle_new_connection(client_socket):
+            message = "What is your nickname?".encode('utf-8')
+            client_socket.sendall(message)
+            message = client_socket.recv(4096)
+            decoded_message = message.decode("utf-8")
+            print(f"The client name is {decoded_message}\n")
+            CONNECTION.append(client_socket)
+            NICKNAME.append(decoded_message)
+            new_connection_message(client_socket,decoded_message)
+            return
+
+
+
+def connect_method():
+    '''
+    This method is used to create a connection for new clients.
+    '''
+    while True:        
+        try:
+            (client_socket, address) = server.accept()
+            handle_new_connection(client_socket)
+            thread = threading.Thread(target=handle_method, args=(client_socket,"Hello from Server"))
+            thread.start()
+        except KeyboardInterrupt:
+            break
+## Creating a new socket connection.
 
 
 
@@ -13,7 +101,12 @@ LOCAL_IP_ADDRESS = '127.0.0.1'
 LOCAL_PORT_ADDRESS = 8080
 # This will allow the port for reconnection
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-## This creates a local server right here.
 server.bind((LOCAL_IP_ADDRESS,LOCAL_PORT_ADDRESS))
 ## How many connections we will support.
 server.listen(5)
+connect_method()
+
+
+
+
+
